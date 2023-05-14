7c0ed8750c1d0da3514822590d7815ec7a8ecbd5
paulamkt <paula@makata.tv>
2023-05-14 11:24:01 -0500

    project start

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './variables.css'
+import Home from './Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Home extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { };
+    }
+  
+    render() {
+      return (
+        <div>
+          <h1>Welcome to Track your flight</h1>
+        </div>
+      );
+    }
+  }
